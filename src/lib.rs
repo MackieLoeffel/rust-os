9fafd125ab61ac190838833d05871273a1f2d093
Mackie Loeffel <mackie.loeffel@web.de>
2016-12-15 23:29:12 +0100

    extracted windows

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod cga_screen;
 mod io_port;
 mod keyboard;
 
-use cga_screen::{SCREEN, Color};
+use cga_screen::{SCREEN, Color, CGAScreen, ROWS, COLUMNS};
 use keyboard::{KEYBOARD};
 
 use core::fmt;
@@ -31,32 +31,7 @@ pub extern fn rust_main() {
         println!(screen, "Key {}", key.ascii());
 
         if key.ascii() == 'q' {
-            screen.set_color(Color::White, Color::Blue);
-            screen.clear();
-            println!(screen, "A problem has been detected and Windows has been shut down to prevent damage");
-            println!(screen, "to your computer.");
-            println!(screen, "");
-            println!(screen, "The problem seems to be caused by the following file: SPCMDCON.SYS");
-            println!(screen, "PAGE_FAULT_IN_NONPAGED_AREA");
-            println!(screen, "If this is the first time you've seen this stop error screen,");
-            println!(screen, "restart your computer. If this screen appears again, follow");
-            println!(screen, "these steps:");
-            println!(screen, "");
-            println!(screen, "Check to make sure any new hardware or software is properly installed.");
-            println!(screen, "If this is a new installation, ask your hardware or software manufacturer");
-            println!(screen, "for any Windows updates you might need.");
-            println!(screen, "");
-            println!(screen, "If problems continue, disable or remove any newly installed hardware");
-            println!(screen, "or software. Disable BIOS memory options such as caching or shadowing.");
-            println!(screen, "If you need to use Safe Mode to remove or disable components, restart");
-            println!(screen, "your computer, press F8 to select Advanced Startup Options, and then");
-            println!(screen, "select Safe Mode.");
-            println!(screen, "");
-            println!(screen, "Technical information:");
-            println!(screen, "");
-            println!(screen, "*** STOP: 0x00000050 (0xFD3094C2,0x00000001,0xFBFE7617,0x00000000)");
-            println!(screen, "");
-            println!(screen, "***  SPCMDCON.SYS - Address FBFE7617 base at FBFE5000, DateStamp 3d6dd67c");
+            windows();
         }
     }
 }
@@ -109,6 +84,37 @@ pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
 
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern "C" fn _Unwind_Resume() -> ! {
+pub extern "C" fn _Unwind_Resume() -> ! { loop {} }
+
+pub fn windows() {
+    let mut screen = CGAScreen::new(0, 0, COLUMNS, ROWS);
+
+    screen.set_color(Color::White, Color::Blue);
+    screen.clear();
+    println!(screen, "A problem has been detected and Windows has been shut down to prevent damage");
+    println!(screen, "to your computer.");
+    println!(screen, "");
+    println!(screen, "The problem seems to be caused by the following file: SPCMDCON.SYS");
+    println!(screen, "PAGE_FAULT_IN_NONPAGED_AREA");
+    println!(screen, "If this is the first time you've seen this stop error screen,");
+    println!(screen, "restart your computer. If this screen appears again, follow");
+    println!(screen, "these steps:");
+    println!(screen, "");
+    println!(screen, "Check to make sure any new hardware or software is properly installed.");
+    println!(screen, "If this is a new installation, ask your hardware or software manufacturer");
+    println!(screen, "for any Windows updates you might need.");
+    println!(screen, "");
+    println!(screen, "If problems continue, disable or remove any newly installed hardware");
+    println!(screen, "or software. Disable BIOS memory options such as caching or shadowing.");
+    println!(screen, "If you need to use Safe Mode to remove or disable components, restart");
+    println!(screen, "your computer, press F8 to select Advanced Startup Options, and then");
+    println!(screen, "select Safe Mode.");
+    println!(screen, "");
+    println!(screen, "Technical information:");
+    println!(screen, "");
+    println!(screen, "*** STOP: 0x00000050 (0xFD3094C2,0x00000001,0xFBFE7617,0x00000000)");
+    println!(screen, "");
+    println!(screen, "***  SPCMDCON.SYS - Address FBFE7617 base at FBFE5000, DateStamp 3d6dd67c");
+
     loop {}
 }
